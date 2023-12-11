15f8930214f8cb7edcc5b7e702d00e074d373ce9
hareshkhan01 <mrhitman060@gmail.com>
2023-12-11 22:19:18 +0530

    This the the commit after create the bottom sheet and set agnle down, tex view, and horizontal three dot

---
+++ app/src/main/java/com/hitman/musicx/ui/MusicBottomSheet.java
@@ -0,0 +1,39 @@
+package com.hitman.musicx.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
+import com.hitman.musicx.R;
+
+
+public class MusicBottomSheet extends BottomSheetDialogFragment {
+    public MusicBottomSheet() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view=inflater.inflate(R.layout.fragment_music_bottom_sheet, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
